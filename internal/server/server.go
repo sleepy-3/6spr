a3c794cc5759f71ece08ce01e02b19e74a9ef2a6
Emil Timinev <emile.t@yandex.ru>
2025-09-08 12:48:54 +0300

    firstrty

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,40 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	logger *log.Logger
+	serv   *http.Server
+}
+
+func (server *Server) Launch() error {
+	return server.serv.ListenAndServe()
+}
+
+func MyServer(logger *log.Logger) *Server {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", handlers.HandleFile)
+	router.HandleFunc("/upload", handlers.HandleUpload)
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		logger: logger,
+		serv:   server,
+	}
+
+}
